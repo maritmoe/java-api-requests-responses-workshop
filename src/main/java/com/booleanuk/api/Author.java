3ff557ca962e729e147074cabd386c712a21cd0f
Marit <marit.moe2@ntebb.no>
2024-01-29 15:40:59 +0100

    Add id to Author.

--- src/main/java/com/booleanuk/api/Author.java
+++ src/main/java/com/booleanuk/api/Author.java
@@ -1,12 +1,20 @@
 package com.booleanuk.api;
 
 public class Author {
+    private static int nextID = 0;
+    private int id;
     private String name;
     private String email;
 
     public Author(String name, String email) {
+        nextID += 1;
+        this.id = nextID;
         this.name = name;
         this.email = email;
+    }
+
+    public int getId(){
+        return this.id;
     }
 
     public String getName() {
